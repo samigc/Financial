ffa4339ef9565582112567de25d3f4bd95fdb4bf
Integro's <jcliberatol@unal.edu.co>
2016-11-18 16:23:33 -0500

    adds file

--- Financial/comparedates.py
+++ Financial/comparedates.py
@@ -5,14 +5,10 @@ import matplotlib.pyplot as plt
 weat = pd.read_csv('cleanwheat.csv')
 otro = pd.read_csv('WIKI/ABFS.csv')
 
-
-#otro.reindex_like(trigo)
-
 weati = weat.set_index('Date')
 otroi = otro.set_index('Date')
 
 otroi = otroi.reindex_like(weati).dropna(how="all")
-#merged = pd.concat([weati,otroi],axis=1,join='inner').shape
 wheat = weati['Price']
 wheat.name = 'wheat'
 otter = otroi['High']
@@ -21,19 +17,3 @@ compare = pd.concat([wheat,otter],axis=1, join='inner')
 
 compare.plot()
 plt.show()
-
-
-# uswt = pd.read_csv('USWHEAT.tsv',sep='\t')
-# li = uswt['Date'][0].split('-')
-# lt = []
-# uswtdat = uswt['Date']
-# for i,row in uswtdat.iteritems():
-# 	print(i,row)
-# 	li = row
-# 	li = li.split('-')
-# 	li = li[2]+'-'+li[0]+'-'+li[1]
-# 	print(li)
-# 	lt.append(li)
-
-# se = pd.Series(lt)
-# uswt['Date'] = se
