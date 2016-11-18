2282af0b51144ac24c9368bf54f1f8966647c177
Integro's <jcliberatol@unal.edu.co>
2016-11-18 14:07:48 -0500

    adds more assets

--- Financial/comparedates.py
+++ Financial/comparedates.py
@@ -1,12 +1,11 @@
 import pandas as pd
-test = pd.read_csv('LSE-WEAT.csv')
-invweat = pd.read_csv('wheat.csv', sep = ';')
+weat = pd.read_csv('LSE-WEAT.csv')
 otro = pd.read_csv('WIKI/ABFS.csv')
 
-print(trigo[trigo.Date > '2010-10-10']['Date'])
 
-otro.reindex_like(trigo)
+#otro.reindex_like(trigo)
 
-trigoi = trigo.set_index('Date')
+weati = weat.set_index('Date')
 otroi = otro.set_index('Date')
 
+otroi = otro.reindex_like(weati).dropna(how="all")
