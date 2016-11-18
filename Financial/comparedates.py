74c0a1af7697b4dff8165f2e37074c121888e3fb
Integro's <jcliberatol@unal.edu.co>
2016-11-18 11:58:11 -0500

    small changes"

--- Financial/comparedates.py
+++ Financial/comparedates.py
@@ -6,3 +6,8 @@ print(trigo.describe())
 print(otro.describe())
 
 print(trigo[trigo.Date > '2010-10-10']['Date'])
+
+otro.reindex_like(trigo)
+
+trigoi = trigo.set_index('Date')
+otroi = otro.set_index('Date')
