37d1c798fa8254328fbe286ecf41ef65465fbedf
Integro's <jcliberatol@unal.edu.co>
2016-11-18 16:06:43 -0500

    new version with new wheat

--- Financial/comparedates.py
+++ Financial/comparedates.py
@@ -2,7 +2,7 @@ import pandas as pd
 from numpy import *
 import math
 import matplotlib.pyplot as plt
-weat = pd.read_csv('LSE-WEAT.csv')
+weat = pd.read_csv('cleanwheat.csv')
 otro = pd.read_csv('WIKI/ABFS.csv')
 
 
@@ -21,3 +21,19 @@ compare = pd.concat([wheat,otter],axis=1, join='inner')
 
 compare.plot()
 plt.show()
+
+
+# uswt = pd.read_csv('USWHEAT.tsv',sep='\t')
+# li = uswt['Date'][0].split('-')
+# lt = []
+# uswtdat = uswt['Date']
+# for i,row in uswtdat.iteritems():
+# 	print(i,row)
+# 	li = row
+# 	li = li.split('-')
+# 	li = li[2]+'-'+li[0]+'-'+li[1]
+# 	print(li)
+# 	lt.append(li)
+
+# se = pd.Series(lt)
+# uswt['Date'] = se
